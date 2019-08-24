6019a45a557d173dfb4f078889b8619a073fc35d
A. Roussi <roussi.abdelghani@threedsecure.com>
2019-08-25 01:39:57 +0200

    [RS-2] : Add simple /health endpoint

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,9 +1,17 @@
 package com.example.demo;
 
+import lombok.Builder;
+import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
+import org.slf4j.MDC;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.Status;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,9 +20,9 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
 }
 
 
@@ -22,10 +30,36 @@ public class DemoApplication {
 @RestController
 class HelloResource {
 
-	@GetMapping("/hello/{name}")
-	public ResponseEntity<String> sayHello(@PathVariable String name) {
-		String greeting = String.format("Hey %s !", name);
-		log.info(" Receive param {}", greeting);
-		return ResponseEntity.ok(greeting);
-	}
+    @Autowired
+    Environment env;
+
+    @GetMapping("/hello/{name}")
+    public ResponseEntity<String> sayHello(@PathVariable String name) {
+        String greeting = String.format("Hey %s !", name);
+        log.info("{} - Receive param {}", greeting, MDC.get("X-B3-TraceId"));
+        return ResponseEntity.ok(greeting);
+    }
+
+    @GetMapping("health")
+    public ResponseEntity<Health> customHealth() {
+        String appName = env.getProperty("spring.application.name");
+        String appVersion = env.getProperty("spring.application.version");
+        Health build =
+                Health.status(Status.UP)
+                        .withDetail("info",
+                                Application.builder()
+                                        .name(appName)
+                                        .version(appVersion)
+                                        .build()
+                        )
+                        .build();
+        return ResponseEntity.ok(build);
+    }
+}
+
+@Builder
+@Data
+class Application {
+    private String name;
+    private String version;
 }
